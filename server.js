d1439c066a7f28240aaf6180d0984822e7a5f3dd
Artur0705 <artlil420@gmail.com>
2022-08-12 16:29:12 +1000

    Added a prompt using inquirer

--- server.js
+++ server.js
@@ -29,4 +29,63 @@ const connection = mysql.createConnection({
     promptStart();
   };
 
-  
+  // Creating a propmt question with inquirer
+  const promptStart = () => {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "choices",
+          message: "What would you like to do?",
+          choices: [
+            "Show all departments",
+            "Show all roles",
+            "Show all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update an employee role",
+            "Update an employee manager",
+            "Show employees by department",
+            "Delete a department",
+            "Delete a role",
+            "Delete an employee",
+            "Show department budgets",
+            "Exit",
+          ],
+        },
+      ])
+      .then(({ choices }) => {
+        choices === "Show all departments"
+          ? showDepartments()
+          : choices === "Show all roles"
+          ? showRoles()
+          : choices === "Show all employees"
+          ? showEmployees()
+          : choices === "Add a department"
+          ? createDepartment()
+          : choices === "Add a role"
+          ? createRole()
+          : choices === "Add an employee"
+          ? addEmployee()
+          : choices === "Update an employee role"
+          ? updateEmployee()
+          : choices === "Update an employee manager"
+          ? updateManager()
+          : choices === "Update an employee manager"
+          ? updateManager()
+          : choices === "Show employees by department"
+          ? showEmployeeDepartment()
+          : choices === "Delete a department"
+          ? deleteDepartment()
+          : choices === "Delete a role"
+          ? deleteRole()
+          : choices === "Delete an employee"
+          ? deleteEmployee()
+          : choices === "Show department budgets"
+          ? showBudget()
+          : choices === "Exit"
+          ? connection.end()
+          : connection.end();
+      });
+  };
